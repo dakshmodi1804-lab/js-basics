465534bee414c28ab0b38291c94090f0a93e85bc
dakshmodi1804-lab <dakshmodi1804@gmail.com>
2026-06-04 11:09:19 +0000

    Datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3)     -- we are using nodejs not browser so cant use alert
+
+console.log("hello")
+// code readablity should be high
+
+//datatypes in js 
+let name="daksh"
+let age=21
+let isloggedin=false;
+
+let state;     // it is undefined
+ //let state=null    // null value 
+
+// number == 2 to the power 53
+// string in ""
+// boolean either true or false
+
+console.log(typeof isloggedin)
