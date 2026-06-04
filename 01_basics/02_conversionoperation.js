d37817b1b00130e2ee60e80e53d16a4268d77f0c
dakshmodi1804-lab <dakshmodi1804@gmail.com>
2026-06-04 12:17:14 +0000

    type conversion

---
+++ 01_basics/02_conversionoperation.js
@@ -0,0 +1,23 @@
+let score=undefined
+
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+
+console.log(valueInNumber)
+
+
+//"33" --  33
+//"33abc" -- Nan (not in number)
+//true -- 1
+// undefined -- Nan
+
+
+
+
+
+//boolean conversion
+//1 -- true     0 -- false
+// "" -- false
+// "daksh" -- true
