73bb364dd214c23537258586d35d0998e8cb9b61
dakshmodi1804-lab <dakshmodi1804@gmail.com>
2026-06-05 10:48:05 +0000

    numbers and maths

---
+++ 01_basics/04_nums_maths.js
@@ -0,0 +1,47 @@
+  const score = 400
+
+  const balance=new Number(100)
+//   console.log(score)
+//   console.log(balance)
+
+//   console.log(balance.toString())               // convert to string to get several operations and functions of string
+
+
+//   console.log(balance.toFixed(2))              // number of places after decimal needed
+
+
+
+
+  const oth=23.8996
+  const ano=123.890
+
+//   console.log(oth.toPrecision(3))                 //precise upto 2 3places and then roundoff
+//   console.log(ano.toPrecision(3))
+
+
+
+
+
+  // ------------------------------------MATHS-----------------------------
+
+//    console.log(Math)
+//    console.log(Math.abs(-4))
+
+//    console.log(Math.ceil(5.2))
+//    console.log(Math.floor(7.8))
+
+//    console.log(Math.max(20,3,45,32,334,543))
+
+console.log(Math.random())             // generate random value bw 0 and 1
+
+console.log(Math.random()*10)           // multiply by number in which range you want
+
+console.log(Math.floor(Math.random()*10))   
+
+
+
+
+const min = 10
+const max=20
+
+console.log(Math.floor(Math.random()* (max-min+1) + min))          // to get random numbers between a certain range formula
