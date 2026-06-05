0bbd60ed35f4e2a242fefdc1f97fea283948fff7
dakshmodi1804-lab <dakshmodi1804@gmail.com>
2026-06-05 08:39:29 +0000

    type conversion

--- 01_basics/02_conversionoperation.js
+++ 01_basics/02_conversionoperation.js
@@ -1,11 +1,11 @@
 let score=undefined
 
-console.log(typeof(score));
+//console.log(typeof(score));
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
-console.log(valueInNumber)
+//console.log(valueInNumber)
 
 
 //"33" --  33
@@ -21,3 +21,21 @@ console.log(valueInNumber)
 //1 -- true     0 -- false
 // "" -- false
 // "daksh" -- true
+
+
+
+let str1 = "Hello"
+let str2 = " Daksh"
+let str3 = str1 + str2
+console.log(str3)
+
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2+2)            // it also converts into string and give 122
+// console.log(1 + 2 + "2")          // it gave 32
+
+
+
+console.log(+true)       // it gave 1 as + tycasted boolean into number
+console.log(+"")        //similarly 0 for false
